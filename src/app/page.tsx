ed1cec8fb9d562e78313d6008bb123d5233f1ed3
Justin Gee <justing33193@gmail.com>
2023-09-02 15:54:02 -0400

    initial start of project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 export default function Home() {
     return (
-        <main>
+        <>
+            <h1 className='font-bold'>Welcome to my homepage!</h1>
             <div>
-                Frontend is working
+                <div className="overflow-hidden h-fit rounded-lg border border-gray-200">
+                    <p>
+                        Portfolio
+                    </p>
+                </div>
+                <div className="overflow-hidden h-fit roundend-lg border border-red-200">
+                    <dl>
+                        <div>
+                            <p>
+                                Project 1
+                            </p>
+                        </div>
+                    </dl>
+                </div>
             </div>
-        </main>
+        </>
     )
 }
